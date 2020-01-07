1ff451b7467ce387ab7d393e9c0a775c27833f5f
Alan Thompson <cloojure@gmail.com>
2020-01-06 22:13:57 -0800

    misc

--- src/clj/demo/core.clj
+++ src/clj/demo/core.clj
@@ -1,4 +1,5 @@
-(ns demo.core )
+(ns demo.core
+  (:use tupelo.core))
 
 (defn atom?
   "Returns true iff arg is a clojure.lang.Atom."
@@ -10,13 +11,74 @@
   "Returns true iff arg is a clojure.lang.Ref."
   [arg] (= (type arg) clojure.lang.Ref))
 
-(defn unvar
-  "When passed a clojure var-object, returns the referenced value (via deref/var-get);
-  else returns arg unchanged."
-  [value-or-var]
-  (if (var? value-or-var)
-    (deref value-or-var) ; or var-get
-    value-or-var))
+(defn ^:no-doc let-tlv-impl-preproc
+  [bindings-user] ; e.g. [a 1 b 2]
+  (let [syms-user       (mapv first (partition 2 bindings-user)) ; e.g. [a b]
+        syms-gen        (mapv #(gensym %) syms-user) ; e.g. [a24120 b24121]
+        bindings-middle (vec (interleave syms-gen syms-user)) ; e.g.  [a24120 a b24121 b]
+        bindings-tlv    (vec (interleave syms-user syms-gen))] ; e.g.  [a a24120 b b24121]
+    (vals->map syms-user syms-gen bindings-middle bindings-tlv)))
+
+(defn ^:no-doc let-tlv-impl
+  [bindings-user  ; e.g. [a 1 b 2]
+   forms] ; e.g. [ (prt @a) (prt @b) ]
+  (let [preproc-info    (let-tlv-impl-preproc bindings-user)
+        bindings-middle (:bindings-middle preproc-info)
+        bindings-tlv    (:bindings-tlv preproc-info)]
+    `(let ~bindings-user
+       (let ~bindings-middle
+         (with-local-vars ~bindings-tlv
+           ~@forms)))))
+
+(defmacro let-tlv
+  [bindings & forms]
+  (let-tlv-impl bindings forms)) ; pass `forms` as a seq, not using `apply`
+
+(defmacro tlv-get
+  [var-sym] `(clojure.core/var-get ~var-sym ))
+
+(defmacro tlv-set-it
+  [ltv-sym & forms]
+  `(let [local-var-obj#   ~ltv-sym
+         ~'it             (clojure.core/deref local-var-obj#)
+      ;  ~ltv-sym         ~'it ; use of target var disabled
+        ]
+     (clojure.core/var-set local-var-obj#
+       (do
+         ~@forms))) )
+
+(defmacro set-it
+  [state & forms]
+  `(do
+    ;(spyx (var? ~state))
+     (cond
+       (atom? ~state) (swap! ~state
+                        (fn [~'it]
+                          ~@forms))
+
+       (agent? ~state) (send ~state
+                         (fn [~'it]
+                           ~@forms))
+
+       (ref? ~state) (alter ~state
+                       (fn [~'it]
+                         ~@forms))
+
+       (var? ~state) (alter-var-root ~state
+                       (fn [~'it]
+                         ~@forms))
+
+       :else (throw (ex-info "Illegal state arg" {:state ~state
+                                                  :type  (type ~state)}))
+       )))
+
+(defmacro set-it-dynamic
+  [var-sym & forms]
+  `(let [~'it ~var-sym]
+     (clojure.core/var-set (var ~var-sym) ~@forms)))
+  (comment  ; alternate implementation
+    (defmacro var-set-dynamic-1
+      [var-sym val] (list 'set! var-sym val)))
 
 (defmacro mylet
   [mysym & forms]
@@ -24,11 +86,37 @@
      (let [~mysym 5]
        ~@forms)))
 
-(defmacro mut!
-  [var-obj-name & forms]
-  `(let [local-var-obj#     ~var-obj-name
-         ~'it               (clojure.core/deref local-var-obj#)
-         ~var-obj-name      ~'it]
-     (clojure.core/var-set  local-var-obj#
-       (do
-         ~@forms))))
+(defmacro var-anon
+  [val]
+  `(def new-var# ~val))
+
+(defn unvar
+  "When passed a clojure var-object, returns the referenced value (via deref/var-get);
+  else returns arg unchanged. Idempotent to multiple calls."
+  [value-or-var]
+  (if (var? value-or-var)
+    (deref value-or-var) ; or var-get
+    value-or-var))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -1,44 +1,12 @@
 (ns tst.demo.core
-  (:use demo.core tupelo.core tupelo.test)
-  (:require
-    [schema.core :as s]
-    ))
-
-(comment
-  (let [counter (atom nil)]
-    (set-it counter 0)
-    (set-it counter (+ it 1))
-    (satyr counter (+ counter 1))
-    )
-  )
+  (:use demo.core tupelo.core tupelo.test))
 
 (def five 5)
-(dotest
-  (isnt (var? 5)) ; 5 is a value
-  (isnt (var? five)) ; auto-deref replaces `five` with `5`
-  (is   (var? (var five)))
-  (is   (var?    #'five)) ; shortcut for above
-  (with-local-vars [x 42]
-    (is (var? x))
-    (is= 42 (unvar x) (deref x) (var-get x) @x))
-
-  (isnt (atom? 5))
-  (is   (atom? (atom 5)))
-  (isnt (atom? (agent 5)))
-
-  (isnt (agent? 5))
-  (is   (agent? (agent 5)))
-  (isnt (agent? (atom 5)))
-
-  (isnt (ref? 5))
-  (is   (ref? (ref 5)))
-  (isnt (ref? (agent 5)))
-  )
 
 (dotest
-  (isnt (spyx (var? five)))
-  (is (spyx (var? (var five))))
-  (is (spyx (var? #'five))) ; => `(var five)`
+  (isnt (var? five))
+  (is (var? (var five)))
+  (is (var? #'five)) ; => `(var five)`
   ;(spyx (bound? six))   ; syntax error compiling at line XXXXX;  Unable to resolve symbol: six in this context
 
   (let [local-five     five
@@ -68,169 +36,187 @@
       (unvar local-var-five) ; here it deref's a Var object
       (unvar (unvar local-var-five))) ; idempotent
 
-    ; These 3 are all equivalent
+    ; These are all equivalent
     (is= 5
-      (deref local-var-five)
       (var-get local-var-five)
-      @local-var-five))
-  )
-
-(defmacro set-it
-  [state & forms]
-  `(do
-     (spyx (var? ~state))
-     (cond
-       (atom? ~state) (swap! ~state
-                        (fn [~'it]
-                          ~@forms))
-
-       (agent? ~state) (send ~state
-                         (fn [~'it]
-                           ~@forms))
-
-       (ref? ~state) (alter ~state
-                       (fn [~'it]
-                         ~@forms))
-
-       (var? ~state) (alter-var-root ~state
-                       (fn [~'it]
-                         ~@forms))
-
-       :else (throw (ex-info "Illegal state arg" {:state ~state
-                                                  :type  (type ~state)}))
-       )))
-
-(defmacro var-set-dynamic-1
-  [var-sym val] (list 'set! var-sym val))
-(defmacro set-it-dynamic
-  [var-sym & forms]
-  `(let [~'it ~var-sym]
-     (clojure.core/var-set (var ~var-sym) ~@forms)))
-(defmacro get-it-local
-  [var-sym] `(clojure.core/var-get ~var-sym ))
-
-
-(defmacro var-anon
-  [val]
-  `(def new-var# ~val))
+      (deref local-var-five)
+      @local-var-five)))
 
 (dotest
-  (nl) (println :-----------------------------------------------------------------------------)
-  (println :ex-1)
-  (prn (macroexpand '(when true 5)))
-  (println :ex-2)
-  (prn (macroexpand '(demo.core/mylet wilma
-                       (+ wilma 3))))
-  (println :ex-2)
-  (pretty (macroexpand '(demo.core/mut! wilma
-                       (+ wilma 3))))
+  (isnt (var? 5)) ; 5 is a value
+  (isnt (var? five)) ; auto-deref replaces `five` with `5`
+  (is (var? (var five)))
+  (is (var? #'five)) ; shortcut for above
+  (with-local-vars [x 42]
+    (is (var? x))
+    (is= 42 (unvar x) (deref x) (var-get x) @x))
 
-  (nl))
+  (isnt (atom? 5))
+  (is (atom? (atom 5)))
+  (isnt (atom? (agent 5)))
 
+  (isnt (agent? 5))
+  (is (agent? (agent 5)))
+  (isnt (agent? (atom 5)))
+
+  (isnt (ref? 5))
+  (is (ref? (ref 5)))
+  (isnt (ref? (agent 5))))
+
+(dotest
+  (when false
+    (nl) (println :-----------------------------------------------------------------------------)
+    (println :ex-1)
+    ; for clojure.core/when & similar, can use short macro name and single-quote for `macroexpand`
+    (prn (macroexpand '(when true 5)))
+
+    ; for user macros, must use fully-qualified macro name and single-quote for `macroexpand`
+    (println :ex-2)
+    (prn (macroexpand '(demo.core/mylet wilma
+                         (+ wilma 3))))))
+
+;---------------------------------------------------------------------------------------------------
 (dotest
   (throws? (set-it {} 5))
 
-  (nl) (println :var-dynamic)
-  (def ^:dynamic fred nil)
-  (let [fn2 (fn []
-              (spyx (var? fred))
-              (spyx (bound? (var fred)))
-              (spyx (thread-bound? (var fred)))
-              (spyx :in fred)
-              ;(set! fred 99)
-              ;(var-set (var fred) 99)
-              (set-it-dynamic fred 77) ; better than:  (set! fred 99)
-              (spyx :out fred)
-              (set-it-dynamic fred (+ fred 11)) ; better than:  (set! fred 99)
-              (spyx :out fred)
-              (set-it-dynamic fred (+ it 11)) ; better than:  (set! fred 99)
-              (spyx :out fred)
-              )]
-    (spyx :base fred)
-    (binding [fred 5]
-      (spyx :before fred)
-      (fn2)
-      (spyx :after fred))
-    (spyx :base fred))
-
-  (nl) (println :var-anon)
   (let [vv (var-anon nil)]
-    (spyx @vv)
+    (is= nil @vv)
     (set-it vv 0)
-    (spyx @vv)
-    (set-it vv (+ it 1))
-    (spyx @vv))
+    (is= 0 @vv)
+    (set-it vv (inc it))
+    (is= 1 @vv))
 
-  (nl) (println :atom)
   (let [counter (atom nil)]
-    (spyx @counter)
+    (is= nil @counter)
     (set-it counter 0)
-    (spyx @counter)
-    (set-it counter (+ it 1))
-    (spyx @counter))
+    (is= 0 @counter)
+    (set-it counter (inc it))
+    (is= 1 @counter))
 
-  (nl) (println :agent)
   (let [counter (agent nil)]
-    (spyx @counter)
+    (is= nil @counter)
     (set-it counter 0)
-    (Thread/sleep 99) (spyx @counter)
-    (set-it counter (+ it 1))
-    (Thread/sleep 99) (spyx @counter))
+    (Thread/sleep 234)
+    (is= 0 @counter)
+    (set-it counter (inc it))
+    (Thread/sleep 234)
+    (is= 1 @counter))
 
-  (nl) (println :ref)
   (let [counter (ref nil)]
-    (spyx (type counter))
-    (spyx @counter)
+    (is= clojure.lang.Ref (type counter))
+    (is= nil @counter)
     (dosync
       (set-it counter 0)
-      (Thread/sleep 99)) (spyx @counter)
+      (is= 0 @counter))
     (dosync
-      (set-it counter (+ it 1)))
-    (Thread/sleep 99) (spyx @counter))
-  )
+      (set-it counter (inc it)))
+    (is= 1 @counter)))
+
+;---------------------------------------------------------------------------------------------------
+(def barney)
+(def betty "Hi")
+(def ^:dynamic pebbles)
+(def ^:dynamic bambam "Bam!")
+
+(dotest
+  ; only vars supplied with an initial value are `bound`
+  (is (bound? #'betty))
+  (is (bound? #'bambam))
+  (isnt (bound? #'barney))
+  (isnt (bound? #'pebbles))
+
+  ; none fo them are `thread-bound` since we haven't used the `binding` form
+  (isnt (thread-bound? #'barney))
+  (isnt (thread-bound? #'betty))
+  (isnt (thread-bound? #'pebbles))
+  (isnt (thread-bound? #'bambam))
+
+  ; Predicate `thread-bound?` returns true iff:
+  ;   (and <var is dynamic>
+  ;        <withing `binding` scope> )
+  (binding [pebbles 3 ; only dynamic vars can be used in a `binding` form
+            bambam  4]
+    (is (bound? #'pebbles)) ; pebbles is now bound (to 3)
+    (is (bound? #'bambam))  ; bambam is still bound, but to a new value 4
+    (is (thread-bound? #'pebbles)) ; both are thread-bound since we...
+    (is (thread-bound? #'bambam)) ;  ...are in the `binding` form
+    (is= 3 pebbles) ; both values can be accessed thru the global symbol...
+    (is= 4 bambam))) ;  ...and we see the dynamic value present
+
+(def ^:dynamic fred nil)
+(dotest
+  (let [dyny-fn (fn []
+                  (is= 5 fred)
+                  (isnt (var? fred)) ; fred resolves to 5, not a Var object
+                  (is (bound? (var fred))) ; the Var object has both a root binding...
+                  (is (thread-bound? (var fred))) ; ...and a thread binding
+
+                  ; 2 old ways to set a dynamic var
+                  (set! fred 22) ; use global symbol
+                  (is= 22 fred)
+                  (var-set (var fred) 33) ; get the Var obj from the global symbol
+                  (is= 33 fred)
+
+                  ; easier, new way to set a dynamic var
+                  (set-it-dynamic fred 77) ; set to a constant
+                  (is= 77 fred)
+                  (set-it-dynamic fred (+ fred 11)) ; update using dynamic global value
+                  (is= 88 fred)
+                  (set-it-dynamic fred (+ it 11)) ; update using `it` placeholder symbol
+                  (is= 99 fred))]
+    (is= nil fred) ; root value
+    (binding [fred 5] ; set fred to 5 in a dynamic frame
+      (is= 5 fred)
+      (dyny-fn) ; function both reads dynamic value 5, then updates it to 99
+      (is= 99 fred))
+    (is= nil fred))) ; outside of `binding`, we are back to the original root value
 
 ;-----------------------------------------------------------------------------
-(defmacro set-it-local
-  [var-obj & forms]
-  `(let [~'it (clojure.core/deref ~var-obj) ; or var-get
-         ]
-     (clojure.core/var-set ~var-obj ~@forms)))
+(dotest
+  (let [tlv-info (let-tlv-impl-preproc (quote [a 1 b 2]))]
+    (with-map-vals tlv-info [syms-user syms-gen bindings-middle bindings-tlv]
+      (is= syms-user (quote [a b]))
+      (is (wild-match? (quote [:* a :* b]) bindings-middle))
+      (is (wild-match? (quote [a :* b :*]) bindings-tlv))))
 
-(dotest-focus
-  (nl) (println :var-local)
-  (with-local-vars [x 1]
-    (is (var? x))
-    (spyx @x)
-    (set-it-local x 3)
-    (spyx (get-it-local x))
-    (set-it-local x (* it 5))
-    (spyx (get-it-local x))
-    (spyx @x)
+  ; Macro `let-tlv` will expand like this:
+  (let [a 1
+        b (+ a 2)]
+    (let [a0 a
+          b0 b]
+      (with-local-vars [a a0
+                        b b0]
+        (is= 1 @a)
+        (is= 3 @b))))
 
-    (nl) (println :-----------------------------------------------------------------------------)
-    (println :ex-3)
-    (pretty (macroexpand '(demo.core/mut! wilma
-                            (+ wilma 3))))
+  (let-tlv [x 5
+            y (+ x 2)] ; can refer to previous locals, unlike `with-local-vars`
+    (is= clojure.lang.Var
+      (type x)
+      (type y))
+    (is= 5 (tlv-get x)) ; or @x
+    (is= 7 (tlv-get y)) ; or @y
+    (tlv-set-it x (inc it)) ; no `tlv-get` required on target var if use pronoun `it`
+    (is= 6 (tlv-get x))
+    (tlv-set-it y (* it (tlv-get x))) ; can use `it` for target; `tlv-get` required for other TLVs
+    (is= 42 @y)
 
-    (spy :x @x)
-    (mut! x 21)
-    (is= 21  @x)
-    (mut! x (* it 2))
-    (is= 42 @x)
-    (mut! x (+ it (+ x 7) (/ it 7) (/ x 21)))
-    (is= 99 @x)
-
-    ) )
-
-
-
+    ; #todo need example with multiple threads to demonstrate thread-local nature
+    ; #todo what happens when you pass a TLV to a function?  via a Future?
+    ))
 
 
-
-
-
-
-
-
+(dotest
+  (let [int-mult (fn [a b]
+                   (assert (and (pos-int? a) (pos-int? b)))
+                   (let-tlv [cum  0
+                             cntr a]
+                     (while (pos? (tlv-get cntr))
+                      ;(spyx [@cum @cntr])  ; can use reader macro `@` to deref
+                       (tlv-set-it cum (+ it b))
+                       (tlv-set-it cntr (dec it)))
+                     (tlv-get cum))) ; can use `tlv-get` or `deref`
+        ]
+    (is= 12 (int-mult 4 3))))
 
 
